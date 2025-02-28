cc55431ab691360d9d5d3a7c23f84675efe8338a
Jafrina <jafrinarahman13@gmail.com>
2025-02-28 14:18:12 -0600

    Initial Commit

---
+++ AI-dvisor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AI-dvisor
+//
+//  Created by Jafrina Rahman on 2/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
