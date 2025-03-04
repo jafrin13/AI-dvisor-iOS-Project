266709dc896052e3a43cd14b5dad0b4feac5555f
Jafrina <jafrinarahman13@gmail.com>
2025-03-04 00:25:34 -0600

    Working on loading animation

---
+++ AI-dvisor/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  AI-dvisor
+//
+//  Created by Jafrina Rahman on 3/2/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
