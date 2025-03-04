696252c4c4240aaad7ff00b528834b2f892a33aa
Stephanie <S.nguyen6734@gmail.com>
2025-03-03 22:10:09 -0600

    Imported fonts for settings page

---
+++ AI-dvisor/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  AI-dvisor
+//
+//  Created by Nguyen, Stephanie V on 3/3/25.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myLabel.font = UIFont(name: "BakbakOne-Regular", size: 18)
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+   
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
