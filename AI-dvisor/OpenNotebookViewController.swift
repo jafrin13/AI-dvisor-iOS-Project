eaa43771d003c7b3527b82cee159c445e42831a3
Stephanie <s.nguyen6734@gmail.com>
2025-03-05 21:19:43 -0600

    finished settings and upload uI (trying to still fix button though)

--- AI-dvisor/OpenNotebookViewController.swift
+++ AI-dvisor/OpenNotebookViewController.swift
@@ -1,42 +1,20 @@
 //
-//  SettingsViewController.swift
+//  OpenNotebookViewController.swift
 //  AI-dvisor
 //
-//  Created by Nguyen, Stephanie V on 3/3/25.
+//  Created by Nguyen, Stephanie V on 3/5/25.
 //
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class OpenNotebookViewController: UIViewController {
 
-
-   
-    @IBOutlet weak var notifications: UILabel!
-   
-    @IBOutlet weak var settings: UILabel!
-    
-    @IBOutlet weak var onButton: UIButton!
-    @IBOutlet weak var offButton: UIButton!
-    
-
-    @IBOutlet weak var logOffButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-            
-    
 
         // Do any additional setup after loading the view.
     }
     
-    
-    
-
-    
-
-   
-   
 
     /*
     // MARK: - Navigation
