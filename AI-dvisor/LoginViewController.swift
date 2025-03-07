ab40514a533aa7e875d63c2836c2c86f233d3ada
Jafrina <jafrinarahman13@gmail.com>
2025-03-06 22:24:23 -0600

    Finished adding firebase.

--- AI-dvisor/LoginViewController.swift
+++ AI-dvisor/LoginViewController.swift
@@ -6,24 +6,46 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
+    let createAccountSegueIdentifier = "CreateAccountSegue"
+    let successfullyLoggedInIdentifier = "SuccessfullyLoggedSegue"
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var loginErrorMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        passwordTextField.isSecureTextEntry = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // If user attempts to login their credentials will be verified by Firebase and then logged in
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        Auth.auth().signIn(withEmail: usernameTextField.text!, password: passwordTextField.text!) {
+            (authResult, error) in
+            if let error = error as NSError? {
+                self.loginErrorMessage.text = "\(error.localizedDescription)"
+            } else {
+                // Perform segue into the home screen only IF login was successful
+                Auth.auth().addStateDidChangeListener() { (auth, user) in
+                    if user != nil {
+                        self.performSegue(withIdentifier: self.successfullyLoggedInIdentifier, sender: nil)
+                        // Successful log in, clear fields
+                        self.passwordTextField = nil
+                        self.usernameTextField = nil
+                    }
+                }
+                self.loginErrorMessage.text = ""
+            }
+        }
     }
-    */
+    
+    // If user does not have an account, this will take them to the VC for account creation
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        self.performSegue(withIdentifier: self.createAccountSegueIdentifier, sender: self)
+    }
 
 }
--- AI-dvisor/ViewController.swift
+++ AI-dvisor/ViewController.swift
@@ -9,13 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let loginSegueIdentifier = "LoginSegue"
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var progressBarTurtle: UIImageView!
+    
+    // Variables for animating the progress bar
+    var timer: Timer?
+    var currentProgress: Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        
+        progressBar.progress = 0.0
+        progressBar.transform = progressBar.transform.scaledBy(x: 1, y: 4)
+        animateProgressBar()
     }
 
+    // Animates the loading bar to completion
+    func animateProgressBar() {
+        // Schedule a timer to fire every 0.1 seconds
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            // Increment the progress value
+            if self.currentProgress < 1.0 {
+                self.currentProgress += 0.01
+                self.progressBar.setProgress(self.currentProgress, animated: true)
+                self.updateTurtlePosition()
+            } else {
+                // Invalidate the timer once the bar is full
+                self.timer?.invalidate()
+                self.timer = nil
+                // Perform the segue to the login screen
+                self.performSegue(withIdentifier: self.loginSegueIdentifier, sender: self)
+            }
+        }
+    }
+    
+    // Animates the turtle to follow the animation of the edge of the loading bar
+    func updateTurtlePosition() {
+            // Calculate the new x pos for the turtle based on progress
+            let progressBarWidth = progressBar.frame.width
+            // Multiply the width by the current progress to get the x-offset
+            let progressX = CGFloat(currentProgress) * progressBarWidth
+            // Adjust the turtle's center.x to the progress bar's x-origin plus the calculated offset
+            progressBarTurtle.center.x = progressBar.frame.origin.x + progressX
+    }
+    
+    // Makes the progress bar visually rounder at the edges
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Round the entire progress view
+        progressBar.layer.cornerRadius = progressBar.frame.height / 2
+        progressBar.clipsToBounds = true
 
+        // Round each subview (this includes the progress and track layers)
+        for subview in progressBar.subviews {
+            subview.layer.cornerRadius = subview.frame.height / 2
+            subview.clipsToBounds = true
+        }
+    }
 }
 
